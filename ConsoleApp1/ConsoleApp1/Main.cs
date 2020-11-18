70b397a84a47629953eaf48f519a10fe15f27af7
Egor <69056881+Egor-Dei@users.noreply.github.com>
2020-11-18 20:42:06 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Properties;
+
+
+namespace ConsoleApp1
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            IAnimal [] animals = new IAnimal[n];
+            animals[1] = new Bird();
+            animals[0] = new Penguin();
+            animals[2] = new Fish();
+            animals[3] = new Mammal();
+            animals[4] = new Dolphin();
+            for (int i = 0; i < n; i++)
+            {
+                animals[i].ToString();
+            }
+            Console.ReadKey();
+        }
+    }
+}
